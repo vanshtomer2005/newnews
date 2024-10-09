244cf8557bd027235a559c3b5d2071a3531d7080
vanshtomer2005 <vanshtomer42@gmail.com>
2024-10-09 15:09:44 +0530

    position edited

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,14 @@
 async function main(params) {
   //Add event listner for hamburger button
   document.querySelector(".hambuger").addEventListener("click", (e) => {
-    document.querySelector(".links").style.right = "62px";
+    document.querySelector(".links").style.left = "244px";
 
     document.querySelector(".cross").style.display = "inline";
     document.querySelector(".hambuger").style.display = "none";
   });
   //Add event listner for cross button
   document.querySelector(".cross").addEventListener("click", () => {
-    document.querySelector(".links").style.right = "-300px";
+    document.querySelector(".links").style.left = "-300px";
     document.querySelector(".hambuger").style.display = "inline";
     document.querySelector(".cross").style.display = "none";
   });
